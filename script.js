ebcbf0aa499564fe1f48bf864abce463b4ebbf23
Vinycius <vinyciuslu@gmail.com>
2026-03-23 19:49:53 -0300

    Feat: Finalizei o projeto e arrumei os bugs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const elementoTexto = document.getElementById("texto");
 
 function escreverTexto() {
     if (indice < mensagem.length) {
-        elementoTexto.inner += mensagem.charAt(indice);
+        elementoTexto.innerHTML += mensagem.charAt(indice);
         indice++
         setTimeout(escreverTexto, 100);
     } else {
@@ -20,7 +20,7 @@ const containerCoracoes = document.getElementById("container-coracoes");
 
 function criarCoracaoFlutuante() {
     const coracao = document.createElement("div");
-    criarCoracaoFlutuante.classList.add("coracao");
+    coracao.classList.add("coracao");
     coracao.innerHTML = Math.random() > 0.5 ? "💕" : "💞";
 
     coracao.style.left = Math.random() * 100 + "vw";
@@ -38,7 +38,7 @@ setInterval(criarCoracaoFlutuante, 350);
 
 // Contador
 const elementoContador = document.getElementById("contador");
-const dataInicio = new Date("2024-07-14T22:10'00");
+const dataInicio = new Date("2024-07-14T22:10:00");
 
 function mostrarContador() {
     elementoContador.style.opacity = 1;
@@ -48,11 +48,11 @@ function atualizarContador() {
     const agora = new Date();
 
     let anos = agora.getFullYear() - dataInicio.getFullYear();
-    let meses = agora.getFullYear() - dataInicio.getFullYear();
-    let dias = agora.getFullYear() - dataInicio.getFullYear();
-    let horas = agora.getFullYear() - dataInicio.getFullYear();
-    let minutos = agora.getFullYear() - dataInicio.getFullYear();
-    let segundos = agora.getFullYear() - dataInicio.getFullYear();
+    let meses = agora.getMonth() - dataInicio.getMonth();
+    let dias = agora.getDate() - dataInicio.getDate();
+    let horas = agora.getHours() - dataInicio.getHours();
+    let minutos = agora.getMinutes() - dataInicio.getMinutes();
+    let segundos = agora.getSeconds() - dataInicio.getSeconds();
 
     if (segundos < 0){
         segundos += 60;
@@ -70,7 +70,7 @@ function atualizarContador() {
     }
 
     if (dias < 0) {
-        const ultimoMes = new Date(agora.getFullYear(), agora.getMouth(), 0);
+        const ultimoMes = new Date(agora.getFullYear(), agora.getMonth(), 0);
         dias += ultimoMes.getDate();
         meses--;
     }
@@ -88,16 +88,16 @@ atualizarContador();
 setInterval(atualizarContador, 1000);
 
 // Texto final
-const elementoTextoFinal = document.gerElementById("texto-final");
+const elementoTextoFinal = document.getElementById("texto-final");
 
-function escreverTExtoFinal() {
+function escreverTextoFinal() {
     const mensagemFinal = "E vou continuar te amando por muito mais tempo...";
     let i = 0;
 
     elementoTextoFinal.innerHTML = "";
 
     function escrever() {
-        if (i< mensagemFina.length) {
+        if (i< mensagemFinal.length) {
             elementoTextoFinal.innerHTML += mensagemFinal.charAt(i);
             i++;
             setTimeout(escrever, 80);
